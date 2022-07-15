26f93aff6da3da67e202731a2e7905b9fa1588b3
huang-liang-liang <2879760254@qq.com>
2022-07-15 13:42:12 +0800

    commit

--- study/vue3.0/src/views/shop.vue
+++ study/vue3.0/src/views/shop.vue
@@ -5,86 +5,95 @@
     </div>
     <div class="suggest">4人评价“不起球”</div>
 
-    <div class="color">
+    <div class="color"  v-for="(json, index) in json1" :key="index">
       <button
-        :class="{ active: selectIndex === index }"
-        @click="select(index)"
-        v-for="(item, index) in json1[0]"
-        :key="index"
+      @click="select(types[index],item)"
+      :class="{ active: condition[types[index]] === item }"
+        v-for="(item, i) in json"
+        :key="i"
       >
         {{ item }}
       </button>
     </div>
 
-    <div class="size">
-      <button
-        :class="{ active: selectSize === index }"
-        @click="select1(index)"
-        v-for="(item, index) in json1[1]"
-        :key="index"
-      >
-        {{ item }}
-      </button>
-    </div>
-
-    <div class="material">
-      <button
-        :class="{ active: selectMaterial === index }"
-        @click="select2(index)"
-        v-for="(item, index) in json1[2]"
-        :key="index"
-      >
-        {{ item }}
-      </button>
-    </div>
-
-    <div class="button">详细信息：【{{}}】</div>
+    <div class="button">详细信息：【{{showGoodsInfo}}】</div>
   </div>
 </template>
 
 <script>
 import { ref, reactive, toRefs } from "vue";
-import { onMounted } from "vue";
+import { onMounted,computed } from "vue";
 import axios from "axios";
 export default {
   setup() {
     const state = reactive({
       json1: [],
-      json2:[],
-      selectIndex:-1,
-      selectSize:-1,
-      selectMaterial:-1,
-      types:[], //保存从json2中获取的动态属性的名称
-      condition:{} // 有3个属性，分别是 color，size material
+      json2: [],
+      selectIndex: -1,
+      types: [], //保存从json2中获取的动态属性的名称
+      condition: {}, // 有3个属性，分别是 color，size material
     });
-  
-   const select=(i)=>{
-    state.selectIndex = i
+
+    const select = (type,item) => {
+    state.condition[type] = item
+    };
+
+   const showGoodsInfo = computed(()=>{
+        return state.json2.filter((e)=>{
+          return diffObjectByKeys(state.condition,e)
+        })
+    })
+    const diffObjectByKeys = (obj1,obj2)=>{
+      let isEqual = true
+      console.log(obj1,obj2);
+       for(let key in obj1){
+        let v = obj1[key]   // red s miande
+        if(obj2[key] && obj2[key] === v){
+          continue
+        }else{
+        return false
+        }
+      }
+      return isEqual
     }
-    const select1=(i)=>{
-    state.selectSize = i
-    }   
-    const select2=(i)=>{
-    state.selectMaterial = i
-    }
-    
-    onMounted(() => {
-      axios
+    onMounted(async () => {
+      await axios
         .get(
           "https://www.fastmock.site/mock/1778ce9775b3b41808bf82c39c9ece0c/axios/shop"
         )
         .then((res) => {
           state.json1 = res.data.json1;
-          state.json2 = res.data.json2
+          state.json2 = res.data.json2;
           console.log(res.data);
-        });
-      state.types = Object.keys(state.json2[0]) ['color','type','mainliao','price']
-      for(let i=0;i<state.types.length;i++){
-      }
 
+                state.types = Object.keys(state.json2[0]); //['color','type','mainliao','price']
+      for (let i = 0; i < state.types.length; i++) {
+        if (!state.json1[i]) return;
+        let propName = state.types[i];
+        Object.defineProperty(state.condition, propName, {
+          value: state.json1[i][0],
+          writable:true,
+          enumerable:true,
+          configurable:true
+        });
+      }
+        });
+      // state.types = Object.keys(state.json2[0]); //['color','type','mainliao','price']
+      // for (let i = 0; i < state.types.length; i++) {
+      //   if (!state.json1[i]) return;
+      //   let propName = state.types[i];
+      //   Object.defineProperty(state.condition, propName, {
+      //     value: state.json1[i][0],
+      //     writable:true,
+      //     enumerable:true,
+      //     configurable:true
+      //   });
+      // }
     });
     return {
-    ...toRefs(state),select,select1,select2
+      ...toRefs(state),
+      select,    
+      showGoodsInfo   
     };
   },
 };
